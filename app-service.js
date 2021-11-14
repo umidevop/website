68ca67f3e97fd74c55e1d14efa763369ff2f197d
Umid <80524527+umidkoderoeoe9dnj@users.noreply.github.com>
2021-11-14 11:49:47 -0500

    Add files via upload

---
+++ app-service.js
@@ -0,0 +1,8 @@
+const hamburgerMenus = document.querySelector(".hamburger-menuz")
+const containerS = document.querySelector(".containerz")
+
+  hamburgerMenus.addEventListener("click", () => {
+  containerS.classList.toggle("active");
+});
+
+
